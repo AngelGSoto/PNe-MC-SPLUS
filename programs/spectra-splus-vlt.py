d460e1d82b1dab523673c48a8e94a06c27226c5c
AngelGSoto <gsoto.angel@gmail.com>
2023-06-14 15:29:42 -0300

    Working on the script

--- programs/spectra-splus-vlt.py
+++ programs/spectra-splus-vlt.py
@@ -130,10 +130,17 @@ mag_err_arr = np.array(mag_err).reshape(8, 12)
 
 # Data of the SPLUs list
 wl_sp = [3485, 3785, 3950, 4100, 4300, 4803, 5150, 6250, 6600, 7660, 8610, 9110]
-wl_sp = np.array(wl_sp).reshape(1, 12)
+# wl_sp = np.array(wl_sp).reshape(1, 12)
+# Repeat the row to create a 2D array with multiple identical rows
+# Create a 2D array with repeated rows
+repeated_wl_sp = np.repeat([np.array(wl_sp)], 8, axis=0)
 
 color = ["#CC00FF", "#9900FF", "#6600FF", "#0000FF", "#009999", "#006600", "#DD8000", "#FF0000", "#CC0066", "#990033", "#660033", "#330034"]
+repeated_color =  np.repeat([color], 8, axis=0)
+# repeated_color_ = repeated_color.reshape(8, 12)
 marker = ["s", "o", "o", "o", "o", "s", "o", "s", "o", "s", "o", "s"] ### tienen todos los filtros
+repeated_marker = np.repeat([marker], 8, axis=0)
+# repeated_marker_ = repeated_marker.reshape(8, 12)
 
 # sys.exit()
 # ff = (10**(-(table["R_psf"][ind] + 2.41) / 2.5)) / 6250.0**2
@@ -151,7 +158,7 @@ marker = ["s", "o", "o", "o", "o", "s", "o", "s", "o", "s", "o", "s"] ### tienen
 
 # Propagation of error
 err_ = []
-for wll, magg, magerr in zip(wl_sp, mag_arr, mag_err_arr):
+for wll, magg, magerr in zip(repeated_wl_sp, mag_arr, mag_err_arr):
     c = (10**(-2.41/2.5)) / wll**2
     b = -(1. / 2.5)
     err = np.sqrt(((c*10**(b*magg))**2)*(np.log(10)*b*magerr)**2)
@@ -191,12 +198,14 @@ ax.set(ylabel=r'F$(\mathrm{10^{-15} erg\ s^{-1} cm^{-2} \AA^{-1}})$')
 #     min_y_lim = min(Flux) 
 #     plt.ylim(ymin=min_y_lim,ymax=max_y_lim)
 # ax.plot(wl, Flux, c = "gray", linewidth=1.3, alpha=0.6, zorder=5)
-for fitname, wl_, FLUX_, wl_filter, mag_arr_, mag_err_arr_, colors, marker_ in zip(fits_, WL, FLUX, wl_sp, mag_arr, err_, color, marker):
+for fitname, wl_, FLUX_, wl_filter, mag_arr_, mag_err_arr_, colors, marker_ in zip(fits_, WL, FLUX, repeated_wl_sp, mag_arr, err_, repeated_color, repeated_marker):
+    print(marker_[13])
     ax.plot(wl_, FLUX_, c = "gray", linewidth=1.3, alpha=0.6, zorder=5)
     F = (10**(-(mag_arr_ + 2.41) / 2.5)) /wl_filter**2
+    #print(wl_filter, mag_err_arr_)
     # # F /= 1e-15
     # #F *= factor
-    ax.scatter(wl_filter, F, c = colors, marker=marker_, s=80, zorder=4)
+    ax.scatter(wl_filter, F, c = colors, s=80, marker = "o", zorder=4)
     # # ax.errorbar(wl_filter, F, yerr=mag_err_arr_, marker='.', fmt='.', color=colors, ecolor=colors, elinewidth=3.9, markeredgewidth=3.2, capsize=10)
     ax.legend()
     plt.tight_layout()
