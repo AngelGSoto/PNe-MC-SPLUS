de1903d4a848b53fbe1a6d7a8408219f795cdb2f
AngelGSoto <gsoto.angel@gmail.com>
2023-06-16 18:16:32 -0300

    Making photo-spectra

--- programs/spectra-splus-vlt.py
+++ programs/spectra-splus-vlt.py
@@ -84,6 +84,7 @@ missing_data_mask = table_['RA'].mask | table_['VLT'].mask
 # Remove rows with missing data from the table
 table_mask = table_[~missing_data_mask]
 print(table_mask)
+table_mask.write('../VLT-XSHOOTER-spectra-splusmag.ecsv', format='ascii.ecsv', overwrite=True)
 # sys.exit()
 WL, FLUX, fits_ = [], [], []
 mag, mag_err = [], []
@@ -164,12 +165,6 @@ for wll, magg, magerr in zip(repeated_wl_sp, mag_arr, mag_err_arr):
     err = np.sqrt(((c*10**(b*magg))**2)*(np.log(10)*b*magerr)**2)
     err_.append(err)
 
-# PLOTS
-fig, ax = plt.subplots(figsize=(12, 9))
-ax.spines["top"].set_visible(False)  
-ax.spines["right"].set_visible(False)
-ax.set(xlim=[3350,9300])
-
 #axis limit
 # mask_lim = (wl > 6100.) & (wl < 6900.)
 # Flux_lim = Flux[mask_lim]
@@ -188,29 +183,37 @@ ax.set(xlim=[3350,9300])
 #     plt.ylim(ymax=cmd_args.ymax)
 
 #plt.ylim(ymin=-50.0,ymax=200)
-ax.set(xlabel='Wavelength $(\AA)$')
-ax.set(ylabel=r'F$(\mathrm{10^{-15} erg\ s^{-1} cm^{-2} \AA^{-1}})$')
-# Flux /=1e-15
-
 
 # if max(Flux) > 9 * np.mean(Flux):
 #     max_y_lim = max(Flux) * .9
 #     min_y_lim = min(Flux) 
 #     plt.ylim(ymin=min_y_lim,ymax=max_y_lim)
 # ax.plot(wl, Flux, c = "gray", linewidth=1.3, alpha=0.6, zorder=5)
-for fitname, wl_, FLUX_, wl_filter, mag_arr_, mag_err_arr_, colors, marker_ in zip(fits_, WL, FLUX, repeated_wl_sp, mag_arr, err_, repeated_color, repeated_marker):
-    print(marker_[13])
+for fitname, wl_, FLUX_, wl_filter, mag_arr_, mag_err_arr_, repeated_color_, repeated_marker_ in zip(fits_, WL, FLUX, repeated_wl_sp, mag_arr, err_, repeated_color, repeated_marker):
+    fig, ax = plt.subplots(figsize=(12, 9))
+    ax.spines["top"].set_visible(False)  
+    ax.spines["right"].set_visible(False)
+    ax.set(xlim=[3350,9300])
+    ax.set(ylim=[-0.01e-12, 0.07e-12])
+    #plt.ylim(ymin=-50.0,ymax=200)
+    ax.set(xlabel='Wavelength $(\AA)$')
+    ax.set(ylabel=r'F$(\mathrm{10^{-15} erg\ s^{-1} cm^{-2} \AA^{-1}})$')
+
     ax.plot(wl_, FLUX_, c = "gray", linewidth=1.3, alpha=0.6, zorder=5)
-    F = (10**(-(mag_arr_ + 2.41) / 2.5)) /wl_filter**2
-    #print(wl_filter, mag_err_arr_)
-    # # F /= 1e-15
-    # #F *= factor
-    ax.scatter(wl_filter, F, c = colors, s=80, marker = "o", zorder=4)
-    # # ax.errorbar(wl_filter, F, yerr=mag_err_arr_, marker='.', fmt='.', color=colors, ecolor=colors, elinewidth=3.9, markeredgewidth=3.2, capsize=10)
-    ax.legend()
+    F = (10**(-(mag_arr_ + 2.41) / 2.5)) / wl_filter**2
+    # Crea un nuevo gráfico
+        #print(wl_filter, mag_err_arr_)
+        # #F *= factor
+    for i in range(len(mag_arr_)):
+        ax.scatter(wl_filter[i], F[i], c = repeated_color_[i], s=80, marker = repeated_marker_[i], zorder=4)#[]
+        ax.errorbar(wl_filter[i], F[i], yerr=mag_err_arr_[i], marker='.', fmt='.', color=repeated_color_[i], ecolor=repeated_color_[i], elinewidth=3.9, markeredgewidth=3.2, capsize=10)
+    # ax.legend()
     plt.tight_layout()
     figpdf = fitname.replace(".fits", ".pdf")
     plt.savefig(figpdf)
+plt.close()
+        
+
 #ax.axvline(4686, color='r', linewidth=0.3, linestyle='-', zorder = 6, label="He II")
 # plt.text(0.70, 0.19, table["ID"].split("R3.")[-1]).replace(".", "-"),
 #              transform=ax.transAxes, fontsize=25, weight='bold')
