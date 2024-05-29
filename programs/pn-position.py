6076fd76fd51e01a5351fc23c99b13d6d0225b85
AngelGSoto <gsoto.angel@gmail.com>
2024-05-29 12:13:29 -0300

    some update

--- programs/pn-position.py
+++ programs/pn-position.py
@@ -1,85 +1,67 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from  astropy.table import Table, vstack, hstack
-import seaborn as sns
 from astropy.coordinates import SkyCoord
-from astropy import units as u
+import astropy.units as u
+import seaborn as sns
 
-#Files
-df_lmc = pd.read_csv('TAP_3_J_A+A_456_451_PNe.csv')
+# Files
+df_lmc = pd.read_csv('TAP_3_J_MNRAS_438_2642_table1.csv')
 df_smc = pd.read_csv('smc-final.csv')
-##Tiles
 df_tiles = pd.read_csv('Tiles-DR4/iDR4_pointings.csv')
 
 # Selecting only tiles of MC
 df_tiles_mc = df_tiles[df_tiles["Field"].str.contains("MC")]
 
-#LMC                
+# LMC
 ra_lmc = df_lmc["_RA"]
 dec_lmc = df_lmc["_DE"]
-icrs_lmc = SkyCoord(ra=ra_lmc*u.degree, dec=dec_lmc*u.degree, frame='icrs')
-gal_lmc = icrs_lmc.galactic
-l_rad_lmc = gal_lmc.l.radian
-l_rad_lmc[l_rad_lmc > np.pi] -= 2. * np.pi
-b_rad_lmc = gal_lmc.b.radian
-b_deg_lmc = b_rad_lmc * (180/np.pi)#gal.b.degree#b_rad * (180/np.pi)
-l_deg_lmc = l_rad_lmc * (180/np.pi)
+icrs_lmc = SkyCoord(ra=ra_lmc.values*u.degree, dec=dec_lmc.values*u.degree, frame='icrs')
 
-#SMC
+# SMC
 ra_smc = df_smc["RAJ2000"]
 dec_smc = df_smc["DEJ2000"]
-icrs_smc = SkyCoord(ra=ra_smc*u.degree, dec=dec_smc*u.degree, frame='icrs')
-gal_smc = icrs_smc.galactic
-l_rad_smc = gal_smc.l.radian
-l_rad_smc[l_rad_smc > np.pi] -= 2. * np.pi
-b_rad_smc = gal_smc.b.radian
-b_deg_smc = b_rad_smc * (180/np.pi)#gal.b.degree#b_rad * (180/np.pi)
-l_deg_smc = l_rad_smc * (180/np.pi)
+icrs_smc = SkyCoord(ra=ra_smc.values*u.degree, dec=dec_smc.values*u.degree, frame='icrs')
 
-# TILES
+# Tiles
 ra_tile = df_tiles_mc["RA"]
 dec_tile = df_tiles_mc["DEC"]
-icrs_tile = SkyCoord(ra=ra_tile*u.degree, dec=dec_tile*u.degree, frame='icrs')
-gal_tile = icrs_tile.galactic
-l_rad_tile = gal_tile.l.radian
-l_rad_tile[l_rad_tile > np.pi] -= 2. * np.pi
-b_rad_tile = gal_tile.b.radian
-b_deg_tile = b_rad_tile * (180/np.pi)#gal.b.degree#b_rad * (180/np.pi)
-l_deg_tile = l_rad_tile * (180/np.pi)
+icrs_tile = SkyCoord(ra=ra_tile.values*u.degree, dec=dec_tile.values*u.degree, frame='icrs')
 
+# Calculate the number of tiles and size of each tile
+num_tiles = len(icrs_tile)
+total_fov_deg = 2  # Total field of view in degrees
+scaling_factor = 12  # Scaling factor to adjust the size of each tile
+tile_side_length_deg = np.sqrt(total_fov_deg / num_tiles) * scaling_factor
 
-#Plotting
-lgd_kws = {'frameon': True, 'fancybox': True, 'shadow': True}
+# Plotting
 sns.set_style('ticks')
-fig, ax = plt.subplots(figsize=(12, 6))
-# fig = plt.figure(figsize=(14,7))
-# ax = fig.add_subplot(1,1,1, projection='aitoff')
-plt.tick_params(axis='x', labelsize=30) 
+fig, ax = plt.subplots(figsize=(18, 9))  # Increase figure size
+plt.tick_params(axis='x', labelsize=30)
 plt.tick_params(axis='y', labelsize=30)
-plt.xlabel(r'$l$(Galactic longitude)[deg]', fontsize=28)
-plt.ylabel(r'$b$(Galactic latitude)[deg]', fontsize=28)
-# ax.set_xlim(-1.2, 5.7)
-# ax.set_ylim(-3.3, 9.2)
-#fig = plt.figure(figsize=(10, 8))
-#ax = fig.add_subplot(111)
-ax.scatter(l_deg_lmc, b_deg_lmc, marker = "o", s = 80, c = sns.xkcd_palette(["forest green"]), edgecolors= "g", zorder = 3, lw=1, alpha = 0.7, label="PNe")
-ax.scatter(l_deg_smc, b_deg_smc, marker = "o", s = 80, c = sns.xkcd_palette(["forest green"]), edgecolors= "g", zorder = 4, lw=1, alpha = 0.7)#, label="No confirmed HASH PNe")
-ax.scatter(l_deg_tile, b_deg_tile, marker = "s", s = 800, c = sns.xkcd_palette(["grey"]), edgecolors= "none", zorder = 1, lw=1, alpha = 0.3)
-#ax.scatter(l_, b_, s = 100, c = sns.xkcd_palette(["purple"]), edgecolors= "purple", zorder = 2, lw=2)
-#ax.scatter(l_deg, b_deg, s = 150, c = sns.xkcd_palette(["azure"]), edgecolors= "b", zorder = 3, lw=2, label="New PN")
+plt.xlabel(r'$l$ (Galactic longitude) [deg]', fontsize=28)
+plt.ylabel(r'$b$ (Galactic latitude) [deg]', fontsize=28)
 
+# Scatter plot for LMC and SMC
+ax.scatter(icrs_lmc.galactic.l.deg, icrs_lmc.galactic.b.deg, marker="o", s=120, c=sns.xkcd_palette(["forest green"]), edgecolors="g", zorder=3, lw=1, alpha=0.7, label="PNe")
+ax.scatter(icrs_smc.galactic.l.deg, icrs_smc.galactic.b.deg, marker="o", s=120, c=sns.xkcd_palette(["forest green"]), edgecolors="g", zorder=4, lw=1, alpha=0.7)
+
+# Plot tiles with larger symbols
+for i in range(num_tiles):
+    ax.add_patch(plt.Rectangle((icrs_tile.galactic.l.deg[i] - tile_side_length_deg / 2, icrs_tile.galactic.b.deg[i] - tile_side_length_deg / 2), tile_side_length_deg, tile_side_length_deg, color=(0.5, 0.5, 0.5), alpha=0.3))
+
+# Annotations
 bbox_props = dict(boxstyle="round", fc="w", ec="0.78", alpha=0.001, pad=0.1)
-plt.text(0.1, 0.8, 'LMC',
-         transform=ax.transAxes, c="gray", weight='bold', fontsize=32.8, zorder = 1, bbox=bbox_props)
+plt.text(0.1, 0.8, 'LMC', transform=ax.transAxes, c="gray", weight='bold', fontsize=38.8, zorder=1, bbox=bbox_props)
+plt.text(0.85, 0.05, 'SMC', transform=ax.transAxes, c="gray", weight='bold', fontsize=38.8, zorder=2, bbox=bbox_props)
 
-plt.text(0.85, 0.05, 'SMC',
-         transform=ax.transAxes, c="gray", weight='bold', fontsize=32.8, zorder = 2, bbox=bbox_props)
-
+# Grid and legend
+lgd_kws = {'frameon': True, 'fancybox': True, 'shadow': True}  # Define legend properties
 ax.grid(True, linestyle='-.', linewidth=0.7)
-ax.legend(prop={'family': 'monospace', 'size': 15}, **lgd_kws)
-#plt.gca().invert_yaxis()
+ax.legend(prop={'family': 'monospace', 'size': 34}, **lgd_kws)
+
 plt.tight_layout()
-fig.savefig("Text/Figs/galactic-coord-pn.pdf")
-plt.clf()
+
+# Save figure
+fig.savefig("galactic-coord-pn.pdf")
 
